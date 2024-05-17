2f4b25b2d8db3fee87afda1960627f5edcc54629
JacksonMHoke <jacksonmhoke@gmail.com>
2024-05-16 17:34:58 -0700

    Added daily solution

---
+++ solutions/1325.cpp
@@ -0,0 +1,14 @@
+// 1325. Delete Leaves With a Given Value
+class Solution {
+public:
+    TreeNode* removeLeafNodes(TreeNode* root, int target) {
+        if (!root)
+            return NULL;
+        root->left=removeLeafNodes(root->left, target);
+        root->right=removeLeafNodes(root->right, target);
+
+        if (!root->left && !root->right && root->val==target)
+            return NULL;
+        return root;
+    }
+};
