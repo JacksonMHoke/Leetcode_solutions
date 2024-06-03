23af5529f3d9c632389aaaa442048a8470bd0b9b
JacksonMHoke <jacksonmhoke@gmail.com>
2024-06-03 14:15:04 -0700

    Added daily solution

---
+++ solutions/2486.cpp
@@ -0,0 +1,12 @@
+// 2486. Append Characters to String to Make Subsequence
+class Solution {
+public:
+    int appendCharacters(string s, string t) {
+        int j=0;
+        for (int i=0; i<s.length(); ++i) {
+            if (s[i]==t[j])
+                j++;
+        }
+        return t.length()-j;
+    }
+};
