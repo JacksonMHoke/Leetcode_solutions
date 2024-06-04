a8f2d118caf9e510aef7d0545ca0f727555d661f
JacksonMHoke <jacksonmhoke@gmail.com>
2024-06-04 03:37:35 -0700

    Added daily solution

---
+++ solutions/409.py
@@ -0,0 +1,5 @@
+# 409. Longest Palindrome
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        odds = sum([v%2 for v in Counter(s).values()])
+        return len(s)-odds+bool(odds)
