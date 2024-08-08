3e7513e3076b5ce5fe07763bee69c3f23eb8329e
JacksonMHoke <jacksonmhoke@gmail.com>
2024-08-07 18:01:02 -0700

    Added daily solution

---
+++ solutions/885.cpp
@@ -0,0 +1,30 @@
+// 885. Spiral Matrix III
+class Solution {
+public:
+    vector<vector<int>> spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
+        vector<vector<int>> rt{{rStart, cStart}};
+        vector<int> dirs{1,0,-1,0,1};
+        int numFilled=1, r=rStart, c=cStart, curr=1, dir=0;
+        for (int i=0; numFilled<rows*cols; ++i) {
+            for (int j=0; j<=i; ++j) {
+                r-=dirs[dir+1];
+                c+=dirs[dir];
+                if (r>=0 && r<rows && c>=0 && c<cols) {
+                    rt.push_back({r, c});
+                    numFilled++;
+                }
+            }
+            dir=(dir+1)%4;
+            for (int j=0; j<=i; ++j) {
+                r-=dirs[dir+1];
+                c+=dirs[dir];
+                if (r>=0 && r<rows && c>=0 && c<cols) {
+                    rt.push_back({r, c});
+                    numFilled++;
+                }
+            }
+            dir=(dir+1)%4;
+        }
+        return rt;
+    }
+};
