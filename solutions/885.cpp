739adca872cd7c1f97beef51d599e12eab601740
JacksonMHoke <jacksonmhoke@gmail.com>
2024-08-07 18:07:04 -0700

    Updated daily solution to be more clean

--- solutions/885.cpp
+++ solutions/885.cpp
@@ -4,26 +4,19 @@ public:
     vector<vector<int>> spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
         vector<vector<int>> rt{{rStart, cStart}};
         vector<int> dirs{1,0,-1,0,1};
-        int numFilled=1, r=rStart, c=cStart, curr=1, dir=0;
+        int numFilled=1, r=rStart, c=cStart, dir=0;
         for (int i=0; numFilled<rows*cols; ++i) {
-            for (int j=0; j<=i; ++j) {
-                r-=dirs[dir+1];
-                c+=dirs[dir];
-                if (r>=0 && r<rows && c>=0 && c<cols) {
-                    rt.push_back({r, c});
-                    numFilled++;
+            for (int k=0; k<2; ++k) {
+                for (int j=0; j<=i; ++j) {
+                    r-=dirs[dir+1];
+                    c+=dirs[dir];
+                    if (r>=0 && r<rows && c>=0 && c<cols) {
+                        rt.push_back({r, c});
+                        numFilled++;
+                    }
                 }
+                dir=(dir+1)%4;
             }
-            dir=(dir+1)%4;
-            for (int j=0; j<=i; ++j) {
-                r-=dirs[dir+1];
-                c+=dirs[dir];
-                if (r>=0 && r<rows && c>=0 && c<cols) {
-                    rt.push_back({r, c});
-                    numFilled++;
-                }
-            }
-            dir=(dir+1)%4;
         }
         return rt;
     }
