d4203eef1fa71a0aed6f52631bd514271bce084a
JacksonMHoke <jacksonmhoke@gmail.com>
2024-08-21 17:30:48 -0700

    Added daily solution

---
+++ solutions/476.cpp
@@ -0,0 +1,9 @@
+// 476. Number Complement
+class Solution {
+public:
+    int findComplement(int num) {
+        unsigned mask = ~0;
+        while (num & mask) mask<<=1;
+        return ~mask&~num;
+    }
+};
