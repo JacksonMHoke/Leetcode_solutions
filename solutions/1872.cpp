ba6e64e72f9a91b205e641aed028c0cd7d590b7f
JacksonMHoke <jacksonmhoke@gmail.com>
2025-05-06 22:04:28 -0700

    Ideas for Stone Game VIII

---
+++ solutions/1872.cpp
@@ -0,0 +1,14 @@
+// 1872. Stone Game VIII
+class Solution {
+    public:
+        int stoneGameVIII(vector<int>& stones) {
+            return 0;
+        }
+    };
+    
+    //19 1 -100 -10 1 5
+    
+    //if game ends with alice, then the difference is the sum of stones that alice picked
+    //if game ends with bob, then the difference is the negative sum of stones that bob picked
+    
+    //max score is either sum of all positive digits or negative sum of all negative digits
