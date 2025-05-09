65dbfdd0956223c1ecda963259f659d5ac847d45
JacksonMHoke <jacksonmhoke@gmail.com>
2025-05-08 22:37:08 -0700

    Added daily solution

--- solutions/1872.cpp
+++ solutions/1872.cpp
@@ -2,19 +2,14 @@
 class Solution {
     public:
         int stoneGameVIII(vector<int>& stones) {
-            vector<int> dp(stones.size(), INT_MIN);
+            int n=stones.size();
+            vector<int> dp(n);
+            dp.back()=0;
             for (int i=1; i<stones.size(); ++i)
                 stones[i]+=stones[i-1];
-    
-            if (stones.size()==2)
-                return stones.back();
-    
-            dp[stones.size()-2]=stones.back();
-            dp.back()=0;
-            for (int i=(int)stones.size()-3; i>=0; --i) {
+            dp[n-2]=stones[n-1];
+            for (int i=n-3; i>=0; --i)
                 dp[i]=max(dp[i+1], stones[i+1]-dp[i+1]);
-            }
-    
             return dp[0];
         }
     };
