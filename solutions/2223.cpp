e36fa775852d9da5e0229bba057275763c5b96b9
JacksonMHoke <jacksonmhoke@gmail.com>
2025-05-10 20:40:19 -0700

    Closer version using KMP array, need to swap to a modified KMP array to get the answer. Right now I am going to the next point needed for string matching, aka the pattern that has been repeating, but what I need for this problem is slighly different. I need to go back to the previous places in the string that match with the prefix at all. This is slightly different than the KMP because we can exclude patterns that don't explicitly match past the border, but in this we still want to tally up those failed matches

--- solutions/2223.cpp
+++ solutions/2223.cpp
@@ -7,27 +7,19 @@ class Solution {
             vector<int> dp(s.length(), -1);
             int curr=-1;
             for (int i=1; i<s.length(); ++i) {
-                while (curr>=0 && s[curr+1]!=s[i])
+                while (curr>=0 && s[curr+1]!=s[i]) {
+                    cout << "FALLING BACK TO " << dp[curr] << endl;
+                    rt+=curr+1;
                     curr=dp[curr];
+                }
                 if (s[i]==s[curr+1])
                     curr++;
                 dp[i]=curr;
             }
-            bool negative=true;
-            for (int i=s.length()-1; i>0; --i) {
-                if (s[i]>=0 && negative==false)
-                    continue;
-                if (dp[i]==-1) {
-                    negative=true;
-                    continue;
-                }
-                curr=i;
-                while (curr>=0) {
-                    curr=dp[curr];
-                    rt+=curr+1;
-                }
-                negative=false;
-    
+            while (curr>=0) {
+                cout << "FALLING BACK TO " << dp[curr] << endl;
+                rt+=curr+1;
+                curr=dp[curr];
             }
             for (int i=0; i<dp.size(); ++i)
                 cout << dp[i] << " ";
