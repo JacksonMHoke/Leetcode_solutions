a52dd5c1af98db99650eaaadbdd89b5e7f28ccd8
JacksonMHoke <jacksonmhoke@gmail.com>
2025-05-11 19:05:38 -0700

    Added daily solution

--- solutions/2223.cpp
+++ solutions/2223.cpp
@@ -1,5 +1,8 @@
 //2223. Sum of Scores of Built Strings
 // Altered KMP attempt
+// Now understand where KMP array fails
+// We miss the bits that arent necessarily matching up till the border, but are a suffix that matches the prefix
+// We need to find a new prev array that points back to those correctly or accounts for them
 class Solution {
     public:
         long long sumScores(string s) {
@@ -9,7 +12,6 @@ class Solution {
             for (int i=1; i<s.length(); ++i) {
                 while (curr>=0 && s[curr+1]!=s[i]) {
                     cout << "FALLING BACK TO " << dp[curr] << endl;
-                    rt+=curr+1;
                     curr=dp[curr];
                 }
                 if (s[i]==s[curr+1])
